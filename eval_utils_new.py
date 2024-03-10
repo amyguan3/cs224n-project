a85a8225ad3e9cd6443e00ab9edd13325a925d3f
Amy Guan <amyguan@stanford.edu>
2024-03-09 17:55:47 -0800

    testing audio

--- eval_utils_new.py
+++ eval_utils_new.py
@@ -56,7 +56,7 @@ def data(dataset):
 
 def model_pipeline(model, processor, baseline=False, verbose=True):
     device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
-    if baseline == False:
+    if baseline == False: # ADAPTER VER, USES ACCELERATOR
         whisper_asr = pipeline(
             "automatic-speech-recognition", model=model, tokenizer=processor.tokenizer, feature_extractor=processor.feature_extractor
         ) # , device=device
@@ -108,16 +108,17 @@ ACCENTS = ["Filipino",
 
 def get_cv_split_mini(accents=ACCENTS):
     # iterable dataset
-    dataset_total = load_dataset("WillHeld/accented_common_voice", split="train", token=True, revision="e5b7f595177ccdb4a599f3589ce01957b0330357", streaming=True)
-    text_column_name = "sentence"
+    dataset_total = load_dataset("WillHeld/accented_common_voice", split="train", token=True, revision="e5b7f595177ccdb4a599f3589ce01957b0330357") # , streaming=True
+    print(dataset_total[0]["audio"])
+    # text_column_name = "sentence"
 
-    dataset_total = dataset_total.shuffle(seed=42, buffer_size=10_000)
-    # dataset_total = dataset_total.take(28_432) # half of total dataset
-    dataset_total = dataset_total.take(24)
-    dataset_total = dataset_total.cast_column("audio", Audio(sampling_rate=16000))
-    dataset_total = dataset_total.map(normalise) # , num_proc=2
-    dataset_total = dataset_total.filter(is_target_text_in_range, input_columns=[text_column_name]) # , num_proc=2
-    dataset_total = dataset_total.filter(lambda example: example['accents'] in accents)
+    # dataset_total = dataset_total.shuffle(seed=42, buffer_size=10_000)
+    # # dataset_total = dataset_total.take(28_432) # half of total dataset
+    # dataset_total = dataset_total.take(24)
+    # dataset_total = dataset_total.cast_column("audio", Audio(sampling_rate=16000))
+    # dataset_total = dataset_total.map(normalise) # , num_proc=2
+    # dataset_total = dataset_total.filter(is_target_text_in_range, input_columns=[text_column_name]) # , num_proc=2
+    # dataset_total = dataset_total.filter(lambda example: example['accents'] in accents)
 
     print("HALF CV DATASET LOADED")
     return dataset_total
