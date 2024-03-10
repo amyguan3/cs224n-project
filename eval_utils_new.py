ed11c45b01ca732987b3098fcf7b7552e528d3e9
Amy Guan <amyguan@stanford.edu>
2024-03-09 23:11:07 -0800

    fix typo

--- eval_utils_new.py
+++ eval_utils_new.py
@@ -104,7 +104,7 @@ def get_cv_split(accents=ACCENTS):
     cv_split = cv_all.train_test_split(test_size=0.5, seed=42) # 28 samples in train
     cv_split = cv_split.map(normalise) # , num_proc=2
     cv_split = cv_split.filter(is_target_text_in_range, input_columns=["sentence"]) # , num_proc=2
-    dataset_total = dataset_total.filter(lambda example: example['accents'] in accents)
+    cv_split = cv_split.filter(lambda example: example['accents'] in accents)
 
     print("CV DATASET LOADED")
 
@@ -118,7 +118,8 @@ def get_cv_split_mini(accents=ACCENTS):
     cv_split = cv_all.train_test_split(test_size=0.5, seed=42) # 28 samples in train
     cv_split = cv_split.map(normalise) # , num_proc=2
     cv_split = cv_split.filter(is_target_text_in_range, input_columns=["sentence"]) # , num_proc=2
-    dataset_total = dataset_total.filter(lambda example: example['accents'] in accents)
+    cv_split = cv_split.filter(lambda example: example['accents'] in accents)
+    
     print("MINI CV DATASET LOADED")
     return cv_split
 
