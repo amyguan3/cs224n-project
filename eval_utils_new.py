83e1c4b0d35caddb884493e9f469875959aa0306
Amy Guan <amyguan@stanford.edu>
2024-03-09 21:19:26 -0800

    testing old cv 2

--- eval_utils_new.py
+++ eval_utils_new.py
@@ -123,7 +123,7 @@ def get_cv_split_mini(accents=ACCENTS):
     dataset_total = dataset_total.cast_column("audio", Audio(sampling_rate=16000))
     dataset_total = dataset_total.map(normalise) # , num_proc=2
     dataset_total = dataset_total.filter(is_target_text_in_range, input_columns=[text_column_name]) # , num_proc=2
-    dataset_total = dataset_total.filter(lambda example: example['accents'] in accents)
+    # dataset_total = dataset_total.filter(lambda example: example['accents'] in accents)
 
     print("HALF CV DATASET LOADED")
     return dataset_total
