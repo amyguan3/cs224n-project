afca314295b32098f2393e1cbedb064c5052c953
Amy Guan <amyguan@stanford.edu>
2024-03-09 23:15:51 -0800

    fix device options

--- baseline_new.py
+++ baseline_new.py
@@ -4,20 +4,23 @@ from transformers import (WhisperProcessor,
 from eval_utils_new import (model_pipeline,
                             evaluate_asr_alt,
                             get_cv_split)
+import torch
 
 def main():
+    device = torch.cuda.current_device() if torch.cuda.is_available() else 'cpu'
+
     # load whisper feature extractor, tokenizer, processor
     model_path = "openai/whisper-large-v2"
     model = WhisperForConditionalGeneration.from_pretrained(model_path)
     processor = WhisperProcessor.from_pretrained(model_path, language="English", task="transcribe")
 
-    pipe = model_pipeline(model, processor, verbose=True)
+    pipe = model_pipeline(model, processor, baseline=True, verbose=True)
 
     print('GETTING DATASET')
-    dataset = get_cv_split() # .to(device)
+    dataset = get_cv_split().to(device)
 
     print('EVALUATING')
-    metrics = evaluate_asr_alt(pipe, dataset["train"], baseline=True, verbose=True)
+    metrics = evaluate_asr_alt(pipe, dataset["train"], verbose=True)
     print(f'METRICS:\n{metrics}')
 
 if __name__ == "__main__":
