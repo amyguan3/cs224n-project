d687a1aaa4033a1e8fed3cf70a7dc1a8837eb129
Amy Guan <amyguan@stanford.edu>
2024-03-14 22:45:14 +0000

    cv eval for later

--- model_eval.py
+++ model_eval.py
@@ -8,7 +8,7 @@ from peft import (PeftModel,
                   PeftConfig)
 from eval_utils import (new_evaluate,
                             attach_peft)
-from data_utils import (load_cv_india_dataset, load_sd_qa_test_dataset, filter_data)
+from data_utils import (load_cv_india_dataset, load_sd_qa_test_dataset, filter_data, load_cv_us_dataset)
 
 # os.system("pip install -q transformers librosa datasets==2.14.6 evaluate jiwer gradio bitsandbytes==0.37 accelerate geomloss gradio torchaudio")
 # os.system("pip install -q git+https://github.com/huggingface/peft.git@main")
@@ -24,16 +24,25 @@ def main():
     print(f'MODEL LOADED {model_path}')
 
     print('GETTING (FILTERED) DATASET')
-    # dataset = load_cv_india_dataset() # pass in cv sources
-    source = "ind_n"
-    target = "usa"
-    dataset = filter_data(load_sd_qa_test_dataset(), source=source, target=target)
-    dataset['test'] = dataset['test'].rename_column(source, "audio")
-    dataset['test'] = dataset['test'].filter(lambda x: x['question'] is not None)
-    print(dataset)
+    # CV
+    dataset = load_cv_india_dataset() # cv: train = 10%, ~ 900; test = 90%, ~ 9k
+    dataset1k = dataset["test"].select(range(1000))
+
+    # US (later)
+    # dataset = load_cv_us_dataset() # cv: train = 10%, ~ 900; test = 90%, ~ 9k
+    # dataset1k = dataset["test"]
+
+    # SD-QA TEST
+    # source = "ind_n"
+    # target = "usa"
+    # dataset = filter_data(load_sd_qa_test_dataset(), source=source, target=target)
+    # dataset['test'] = dataset['test'].rename_column(source, "audio")
+    # dataset['test'] = dataset['test'].filter(lambda x: x['question'] is not None)
+    # print(dataset)
 
     print('EVALUATING')
-    wer = new_evaluate(model, dataset["test"])
+    # wer = new_evaluate(model, dataset["test"])
+    wer = new_evaluate(model, dataset_1k)
     print(f'MODEL {i} WER: {wer}\n')
     wers.append(wer)
 
